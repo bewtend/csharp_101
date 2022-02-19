2e254f019f111d4e49f1c09665ec7a690d5af99c
Bew <scotflexdev@gmail.com>
2022-02-19 17:16:27 +0300

    add lesson 3 type conversion

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace csharp_learning
         {
             // variables.variable(); 
             // operators.fOperators();
+            type_conversion.imp_conversion();
+            type_conversion.exp_conversion();
         }
     }
 }
---
+++ lesson_3_type_conversion.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace csharp_learning
+{
+    public class type_conversion
+    {
+        //? Implicit Conversion 
+        public static void imp_conversion()
+        {
+            byte _byte = 5;
+            sbyte _sbyte = 10;
+            short _short = 20;
+            int _int = 40;
+
+            _int = _byte + _sbyte + _short + _int;
+
+            long _long = _byte + _sbyte + _short + _int;
+            float _float = _byte + _sbyte + _short + _int + _long;
+            System.Console.WriteLine(_float);
+        }
+
+        //? Explicit Conversion
+        public static void exp_conversion()
+        {
+            int _int = 5;
+            byte _byte = (byte)_int; //? cast etmeye çalışır.
+
+
+            float _float = 10f;
+            _byte = (byte)_float;
+
+
+            string _string = _int.ToString();
+            _string = Convert.ToString(_int);
+
+            _string = "10";
+
+            _int = Convert.ToInt32(_string);
+
+            int result = _int + 5; //* Result = 5;
+
+            _int = Int32.Parse(_string);
+
+        }
+    }
+}
