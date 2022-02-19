345c919a3303957e369955d7f8c0d93748c70011
Bew <scotflexdev@gmail.com>
2022-02-19 16:40:35 +0300

    add operators

---
+++ lesson_1_variables.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace csharp_learning
+{
+    public class variables
+    {
+        public static void fVariable()
+        {
+            // byte _byte = 1;         //? 1byte
+            // sbyte _sbyte = 1;       //? 1byte
+            // short _short = 1;       //? 2byte
+            // ushort _ushort = 1;     //? 2byte
+            Int16 _int16 = 1;          //? 2byte
+            int _int = 1;              //? 4byte 
+            Int32 _int32 = 1;          //? 4byte
+            // Int64 _int64 = 1;       //? 8byte
+            // uint _uint = 1;         //? 4byte
+            // long _long = 1;         //? 8byte
+            // ulong _ulong = 1;       //? 8byte
+            // float _float = 1;       //? 4byte
+            // double _double = 1;     //? 8byte
+            // decimal _decimal = 1;   //? 16byte
+            // char _char = 'c';       //? 2byte
+            string _string = "str";    //? infinity
+            bool _bool = true;
+            // bool _boolean = false;
+            DateTime _dateTime = DateTime.Now;
+            Console.WriteLine(_dateTime);
+            object _object = 'a';
+            object _object1 = 1;
+            object _object2 = DateTime.Now;
+            object _object3 = 0.1;
+            // var _var = 'a';
+            // var _var1 = 1;
+            // var _var2 = 2;
+            dynamic f = 1;
+            _int = _string.Length;
+            _bool = _string.Length > 0;
+            _string = _int.ToString();
+            _string = (_int16 + _int32).ToString();
+            System.Console.WriteLine(_string);
+            System.Console.WriteLine(_dateTime.ToString("d"));
+        }
+    }
+}
---
+++ lesson_2_operators.cs
@@ -0,0 +1,69 @@
+using System;
+
+namespace csharp_learning
+{
+    public class operators
+    {
+        public static void fOperators()
+        {
+
+            //? Atama
+            int x = 1;
+            int y = 2;
+            int z = x + y;
+            z = (x + 2) + (x + 4);
+            z++;
+            z += z;
+            z = +z;
+
+            //? Mantıksal
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+            if (isSuccess && isCompleted)
+            {
+                System.Console.WriteLine("Burak");
+            }
+            else if (isSuccess || isCompleted)
+            {
+                System.Console.WriteLine("not Burak");
+            }
+            else
+            {
+                System.Console.WriteLine("Who are u?");
+            }
+
+            //? İlişkisel
+            //*   <, >, >=, <=, ==, != 
+
+            int num1 = 1;
+            int num2 = 2;
+
+            bool result = num1 > num2;      //* Result = false
+            bool result1 = num1 < num2;     //* Result = true
+            bool result2 = num1 == num2;    //* Result = false
+            bool result3 = num1 >= num2;    //* Result = false
+            bool result5 = num1 <= num2;    //* Result = false
+            bool result7 = num1 != num2;    //* Result == true
+
+            //*  *, /, +, -
+
+            int num3 = 3;
+            int num4 = 4;
+
+            num1 = num3 + num4;
+            num1 = num3++;
+            num1 += num3;
+            num1++;
+
+
+            //* % Mod alır kalanı verir.
+            //* tip: bir sayının modu 2 alınırsa ve sonuö 0 çıkarsa eğer o sayı çifttir.
+            //* Örnek : 12 % 2 == 0 ise çifttir.
+
+            int num12 = 12;
+
+            if (num12 % 2 == 0) Console.WriteLine(num12.ToString() + " is double.");
+        }
+    }
+}
